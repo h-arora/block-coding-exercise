3ac78a7ab79c58bcf43315cc31d5a162b49e31db
Himanshu Arora <himanshuarora@Himanshus-MBP.attlocal.net>
2022-10-02 21:34:49 -0400

    coding problem for block renovation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'block-coding-exercise'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('block-coding-exercise');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('block-coding-exercise app is running!');
-  });
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'block-coding-exercise';
+
+  _items: string[] = ["red", "blue", "red", "green", "blue"];
+  _order: string[] = ["blue", "red", "black"];
+
+
+  set items(val) {
+    this._items = (val as unknown as string).split(',');
+    this.resetResult();
+  }
+
+  get items() {
+    return this._items;
+  }
+
+  set order(val) {
+    this._order = (val as unknown as string).split(',');
+    this.resetResult();
+  }
+
+  get order() {
+    return this._order;
+  }
+
+
+
+  result: string[] = [];
+
+
+  updateResult() {
+    this.result = this.getFilteredResultEfficiently(this.items, this.order);
+  }
+
+  resetResult() {
+    this.result = [];
+  }
+
+  /**
+   * Brute force solution which would work well only for small data sets and would not scale efficiently.
+   * This uses a nested loop and hence the solution uses O(n^2) time complexity thus is not performant for larger data sets
+   */
+  getFilteredResult(items: string[], order: string[]): string[] {
+    const result: string[] = [];
+
+    order.forEach((color) => {
+
+      for (let i = 0; i < items.length; i++) {
+        if (items[i] === color) {
+          result.push(items[i])
+        }
+      }
+
+    })
+    return result;
+  }
+
+
+  /**
+   * This solution is more efficient in terms of time complexity than the previous one as it traverses both arrays once.
+   * We could say this would be roughly a O(2n) solution
+   */
+  getFilteredResultEfficiently(items: string[], order: string[]): string[] {
+    const result: string[] = [];
+
+    const colorCounts: { [name: string]: number } = {};
+
+    // iterate over the items array and form a bucket. This runs n times (where n is the length of items array)
+    items.forEach((color) => {
+      if (colorCounts[color]) {
+        // increment count
+        colorCounts[color] += 1;
+      } else {
+        // initialize
+        colorCounts[color] = 1;
+      }
+    });
+
+    console.log(colorCounts);
+
+    // now iterate over the order array and lookup in the bucket. This runs k times (where k is the length of order array)
+    order.forEach((orderColor) => {
+
+      if (colorCounts[orderColor]) {
+        // this will run variable times based on the count/duplicacy of each color in the items array
+        // we can get rid of this loop if we choose to store the array of colors instead of the count
+        for (let i = 1; i <= colorCounts[orderColor]; i++) {
+          result.push(orderColor);
+        }
+      }
+
+    })
+    return result;
+  }
+
+
 }
